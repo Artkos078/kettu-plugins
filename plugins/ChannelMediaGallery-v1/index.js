026457f763c1b7438bc477689cee2bc532423671
Artkos078 <mbellsloan@icloud.com>
2026-09-07 14:19:19 -0600

    Hide startup messages for plugins

--- plugins/ChannelMediaGallery-v1/index.js
+++ plugins/ChannelMediaGallery-v1/index.js
@@ -766,8 +766,7 @@
     restorePinnedCache();
     if (timer) clearInterval(timer);
     timer = setInterval(rememberCurrentChannel, 1500);
-    toast("Channel Media Gallery loading after restart...");
-    delay(5000).then(function () { rememberCurrentChannel(); toast("Channel Media Gallery loaded"); }).catch(function () { toast("Channel Media Gallery loaded"); });
+    delay(5000).then(function () { rememberCurrentChannel(); }).catch(function () {});
   }
   function onUnload() { if (timer) clearInterval(timer); timer = null; toast("Channel Media Gallery unloaded"); }
   return { onLoad: onLoad, onUnload: onUnload, start: onLoad, stop: onUnload, settings: Settings };
